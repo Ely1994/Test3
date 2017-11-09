dfbfcf0cf88961b5c12548170dcad61f5dcbe085
Elylelf <elylelf@gmail.com>
2017-11-09 15:29:09 +0100

    v2.0

--- test4.php
+++ test4.php
@@ -1,8 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h2>Coucou les boys, bienvenue dans mon exercice debutor3000</h2>
+<p><strong>Enonce :</strong> Creer un tableau (ici 200 cases) avec des valeurs aléatoires de 1 à 100. Parcourir le tableau pour afficher le nombre d'itérations de chaque valeur.</p>
 <?php
-    $x = rand(1,100);
-    $y = rand(1,100);
-    $z = rand(1,100);
-    $tab = array($x, $y, $z);
+    $tab = array();
+    for($j=1; $j<201; $j++) { /* création du tableau */
+        array_push($tab, rand(1,100));
+    }
     $num = 0;
     for($i=1; $i<101; $i++) {
         foreach($tab as $var) {
@@ -10,6 +21,9 @@
                 $num++;
             }
         }
-        echo "La valeur ".$i." est présente ".$num." fois.<br />";
+        echo "La valeur ".$i." est presente ".$num." fois.<br />";
         $num = 0;
-    } cebyfueyueu
+    }
+?>
+</body>
+</html>
